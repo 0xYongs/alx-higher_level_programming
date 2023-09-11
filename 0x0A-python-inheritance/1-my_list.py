408f3042f98bb466934d6deacc212a4c28c142ea
0xYongs <tradingsensei21@gmail.com>
2023-09-11 03:53:03 -0700

    Fixing pyscodestyling

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''contains the MyList class'''
 
+
 class MyList(list):
     '''represents MyList'''
 
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''This module contains the function is_same_class'''
 
+
 def is_same_class(obj, a_class):
     '''return true if object is the exact class a_class, otherwise false'''
     return (type(obj) == a_class)

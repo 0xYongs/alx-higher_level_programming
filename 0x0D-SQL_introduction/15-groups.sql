c03c46b32365b6c50269ab120c52c120d124fe90
0xYongs <tradingsensei21@gmail.com>
2023-10-17 12:59:05 -0700

    Write a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,8 @@
-k-- a script lists the number of records with the same score in the table second_table
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+-- script that shows the coincidences --
+SELECT 
+    score, 
+    COUNT(score) as number
+FROM
+    second_table
+GROUP BY score
+ORDER BY score DESC;

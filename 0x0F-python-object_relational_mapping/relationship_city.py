3936057bb05ea03bc8c651f8709c6f64432b40c0
0xYongs <tradingsensei21@gmail.com>
2023-11-19 04:51:24 -0800

    Write a script that lists all City objects from the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,7 +5,7 @@ contains the class City
 
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from relationship_state import Base, State
 
 
 class City(Base):
